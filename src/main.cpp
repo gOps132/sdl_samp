97f5ef174b9d29c372c311b100edf876073743f2
gOps132 <epilred@icloud.com>
2022-10-23 22:38:45 +0800

    finally got it working with git modules

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
-#include <SDL.h>
+#include "SDL.h"
 
 int main(int argc, char **argv)
 {
-	
+
 	// intialize sdl subsystems
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
 	{
@@ -18,27 +18,26 @@ int main(int argc, char **argv)
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
 		600, 400,
-		SDL_WINDOW_SHOWN
-	);
+		SDL_WINDOW_SHOWN);
 
-    if(!window)
-    {
-        std::cout << "Failed to create window\n";
-        return -1;
-    }
+	if (!window)
+	{
+		std::cout << "Failed to create window\n";
+		return -1;
+	}
 
-    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+	SDL_Surface *window_surface = SDL_GetWindowSurface(window);
 
-    if(!window_surface)
-    {
-        std::cout << "Failed to get the surface from the window\n";
-        return -1;
-    }
+	if (!window_surface)
+	{
+		std::cout << "Failed to get the surface from the window\n";
+		return -1;
+	}
 
-    SDL_UpdateWindowSurface(window);
+	SDL_UpdateWindowSurface(window);
 
-    SDL_Delay(5000);
-	
+	SDL_Delay(5000);
+
 	// SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
 	// SDL_SetRenderDrawColor(renderer, 0,255,0,255);
